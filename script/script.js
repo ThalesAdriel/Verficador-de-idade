16b477b6e7536764d62c96d00bf7b45ace2544b2
ThalesAdriel <115805292+ThalesAdriel@users.noreply.github.com>
2022-12-23 10:28:01 +0000

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -7,8 +7,10 @@ function verificar(){
     let genero = ''
     let img = document.createElement('img')
     let random = Math.floor(Math.random() * 3) + 1
+        
     img.setAttribute('id','photo')
     img.setAttribute('width','300')
+
     if ((ano_nascimento.value) == '' || (ano_nascimento.value) > ano_atual){
         alert('[erro] por favor insira os dados corretamente.')
     }else{
@@ -17,38 +19,42 @@ function verificar(){
             if (idade > 0 && idade <= 12){
                 //criança
                 img.setAttribute('src',`assets/male/crianca-male${random}.jpeg`)
-                
-            } else if (idade <= 21){
+            }   else if (idade <= 21){
                 //adolescente
                 img.setAttribute('src',`assets/male/ado-male${random}.jpeg`)
-            } else if (idade <= 60){
+            }   else if (idade <= 60){
                 //adulto
                 img.setAttribute('src',`assets/male/adulto-male${random}.jpeg`)
             }   else if (idade <= 110){
                 //idoso
                 img.setAttribute('src',`assets/male/idoso-male${random}.jpeg`)
-                
+            }   else if (idade > 110){
+                //mumia
+                genero = "Mumia"
+                img.setAttribute('src',`assets/joke/mumia${random}.gif`)
             }
         }else{
             genero = 'Mulher'
             if (idade > 0 && idade <= 12){
                 //criança
                 img.setAttribute('src',`assets/female/crianca-female${random}.jpeg`)
-            } else if (idade <= 21){
+            }   else if (idade <= 21){
                 //adolescente
                 img.setAttribute('src',`assets/female/ado-femele${random}.jpeg`)
                 
-            } else if (idade <= 60){
+            }   else if (idade <= 60){
                 //adulto
                 img.setAttribute('src',`assets/female/adulto-femele${random}.jpeg`)
             }   else if (idade <= 110){
                 //idoso
                 img.setAttribute('src',`assets/female/idoso-femele${random}.jpeg`)
-                
+            }   else if (idade > 110){
+                //mumia
+                genero = "Mumia"
+                img.setAttribute('src',`assets/joke/mumia${random}.gif`)
             }
         }
         resposta.innerHTML = `Detectamos: ${genero} com ${idade} anos de idade`
         resposta.appendChild(img)
-        
     } 
 }
